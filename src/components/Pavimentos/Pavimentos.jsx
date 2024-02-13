b390962c569acd9656aa3122a7692d651f003a0b
Gerardo Dorado <gerardojur@gmail.com>
2024-02-13 16:57:32 -0500

    configurar cálculos en pavimentos

--- src/components/Pavimentos/Pavimentos.jsx
+++ src/components/Pavimentos/Pavimentos.jsx
@@ -1,7 +1,191 @@
 import { Helmet } from "react-helmet";
 import imagenPav from "../../assets/img/pavimento-aashto.png";
+import { useState } from "react";
 
 const Pavimentos = () => {
+  const [nese_, setNese] = useState("");
+  const [confiabilidad_, setConfiabilidad] = useState("");
+  const [tipoCarretera_, setTipoCarretera] = useState("");
+  const [moduloCapa1_, setModuloCapa1] = useState("");
+  const [moduloCapa2_, setModuloCapa2] = useState("");
+  const [moduloCapa3_, setModuloCapa3] = useState("");
+  const [moduloCapa4_, setModuloCapa4] = useState("");
+  const [m_, setM] = useState("");
+  
+  const espesoresCapas = [
+    [5, 7.5, 10, 12.5, 13, 14, 15, 16, 17, 18, 19, 20],
+    [15, 20, 25, 30],
+    [15, 20, 25, 30, 35, 40, 45, 50, 55, 60],
+  ];
+
+  // Función para manejar el cambio en el input
+  const handleChange = (e, setValue) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+  };
+
+  // CLASES
+  class CapaPavimento {
+    constructor(moduloMpa, m = 1) {
+      this.moduloPsi = moduloMpa * 145; // Módulo de elasticidad de la capa en MPa y lo convierte a psi
+      this.m = m; // Coeficiente de drenaje
+    }
+
+    // Método para resolver una capa de pavimento asfáltico con el método AASHTO-93
+    resolverCapa(
+      nese,
+      zr,
+      carreteraGrande,
+      capa,
+      moduloInfMpa,
+      SNcorregidoAnterior = 0
+    ) {
+      // Método de cálculo del número estrutural (SN) para la capa
+      let moduloInfPsi = moduloInfMpa * 145; // Módulo de elasticidad de la capa inferior en MPa y lo convierte a psi
+      const tolerancia = 1; // se tolera 1% de error
+      let error; // Error porcentual entre la aproximación calculada y la asumida
+      let perdidaServ = carreteraGrande ? 1.7 : 2.2; // Pérdida de serviciabilidad del pavimento
+      // let i = 0; // Conteo de iteraciones
+      let semillaSN = 4.0; // SN semilla asumido para iniciar la iteración actual
+      let calcSN; // SN calculado
+      const s0 = 0.45; // Error estándar
+
+      do {
+        calcSN =
+          -1 +
+          10 **
+            ((Math.log10(nese) +
+              zr * s0 -
+              2.32 * Math.log10(moduloInfPsi) +
+              8.27 -
+              Math.log10(perdidaServ / 2.7) /
+                (0.4 + 1094 / (semillaSN + 1) ** 5.19)) /
+              9.36);
+        error = (Math.abs(calcSN - semillaSN) / calcSN) * 100;
+        semillaSN = calcSN;
+        // i++;
+      } while (error > tolerancia); // Las iteraciones terminan cuando el error no sea mayor que la tolerancia
+
+      this.SNe = Math.round(calcSN * 100) / 100; // asigna SNe a calcSN redondeado a dos decimales
+
+      // Calcular el coeficiente estructural de la capa (a)
+      let a;
+      switch (capa) {
+        case 1:
+          a =
+            Math.round((0.184 * Math.log(this.moduloPsi) - 1.9547) * 100) / 100;
+          break;
+        case 2:
+          a =
+            Math.round((0.249 * Math.log10(this.moduloPsi) - 0.977) * 100) /
+            100;
+          break;
+        case 3:
+          a =
+            Math.round((0.227 * Math.log10(this.moduloPsi) - 0.839) * 100) /
+            100;
+          break;
+      }
+      // Cálculo del espesor constructivo de la capa de pavimento
+      let dCalcPulg = (this.SNe - SNcorregidoAnterior) / (a * this.m); // Espesor calculado en pulgadas
+      let dCalcCm = dCalcPulg * 2.54; // Espesor calculado en centímetros
+      this.dCorregidoCm = espesoresCapas[capa - 1].find(
+        (el) => el > dCalcCm
+      ); // Espesor con redondeo constructivo en centímetros
+      let dCorregidoPulg = Math.round((this.dCorregidoCm / 2.54) * 100) / 100; // Espesor anterior en pulgadas con dos decimales
+      this.SNcorregido =
+        Math.round((SNcorregidoAnterior + a * this.m * dCorregidoPulg) * 100) /
+        100; // SN corregido
+    }
+  }
+
+  // FUNCIONES
+  const leerDatosEntrada = () => {
+    const desvNormalEstandar = [
+      { r: 50.0, zr: 0.0 },
+      { r: 60.0, zr: 0.253 },
+      { r: 70.0, zr: 0.524 },
+      { r: 75.0, zr: 0.674 },
+      { r: 80.0, zr: 0.841 },
+      { r: 85.0, zr: 1.037 },
+      { r: 90.0, zr: 1.282 },
+      { r: 95.0, zr: 1.645 },
+      { r: 98.0, zr: 2.054 },
+      { r: 99.0, zr: 2.327 },
+      { r: 99.9, zr: 3.09 },
+      { r: 99.99, zr: 3.75 },
+    ];
+
+    const nese = parseFloat(nese_) * 1e6;
+    const confiabilidad = parseFloat(confiabilidad_);
+    const carreteraGrande = tipoCarretera_ === "grande";
+    const moduloCapa1 = parseFloat(moduloCapa1_);
+    const moduloCapa2 = parseFloat(moduloCapa2_);
+    const moduloCapa3 = parseFloat(moduloCapa3_);
+    const moduloCapa4 = parseFloat(moduloCapa4_);
+    const m = parseFloat(m_);
+    const zr = desvNormalEstandar.find((el) => el.r === confiabilidad).zr;
+
+    return [
+      moduloCapa1,
+      moduloCapa2,
+      moduloCapa3,
+      moduloCapa4,
+      m,
+      nese,
+      zr,
+      carreteraGrande,
+      confiabilidad,
+    ];
+  };
+
+  const crearYresolverCapas = (
+    moduloCapa1,
+    moduloCapa2,
+    moduloCapa3,
+    moduloCapa4,
+    m,
+    nese,
+    zr,
+    carreteraGrande
+  ) => {
+    // Crear capas-------------------------------------------------------------------------
+    const capas = [
+      new CapaPavimento(moduloCapa1),
+      new CapaPavimento(moduloCapa2, m),
+      new CapaPavimento(moduloCapa3, m),
+    ];
+    // Resolver capas----------------------------------------------------------------------
+    capas[0].resolverCapa(nese, zr, carreteraGrande, 1, moduloCapa2);
+    capas[1].resolverCapa(
+      nese,
+      zr,
+      carreteraGrande,
+      2,
+      moduloCapa3,
+      capas[0].SNcorregido
+    );
+    capas[2].resolverCapa(
+      nese,
+      zr,
+      carreteraGrande,
+      3,
+      moduloCapa4,
+      capas[1].SNcorregido
+    );
+
+    return capas;
+  };
+
+  const calcular = () => {
+    // Lee los datos de entrada con la función y los guarda en un array
+    const datosEntradaLeidos = leerDatosEntrada()
+    // Obtiene las capas resueltas a partir de los datos leídos y las guarda en un array
+    const capas = crearYresolverCapas(...datosEntradaLeidos)
+    // el operador ... (spread) entrega los datos por separado del array de datos leídos
+    console.log(capas)
+  };
+
   return (
     <main id="main-pavimentos">
       <Helmet>
@@ -29,6 +213,7 @@ const Pavimentos = () => {
           La guía completa del método AASHTO-93 se encuentra{" "}
           <a
             target="_blank"
+            rel="noreferrer"
             href="https://drive.google.com/file/d/1VH4g-RPlbb6BOuhowi4yz0bk4UuHhd0O/view?usp=sharing"
           >
             aquí
@@ -40,7 +225,7 @@ const Pavimentos = () => {
         <h2>Datos de entrada</h2>
         <form>
           <div>
-            <label for="nese">N en millones:</label>
+            <label htmlFor="nese">N en millones:</label>
             <input
               type="number"
               id="nese"
@@ -48,10 +233,18 @@ const Pavimentos = () => {
               max="100"
               step="0.01"
               placeholder="Ej: 4.5"
+              value={nese_}
+              onChange={(e) => handleChange(e, setNese)}
               required
             />
-            <label for="R">Confiabilidad (%):</label>
-            <select name="confiabilidad" id="R" required>
+            <label htmlFor="R">Confiabilidad (%):</label>
+            <select
+              name="confiabilidad"
+              id="R"
+              value={confiabilidad_}
+              onChange={(e) => handleChange(e, setConfiabilidad)}
+              required
+            >
               <option value="50">50</option>
               <option value="60">60</option>
               <option value="70">70</option>
@@ -65,12 +258,18 @@ const Pavimentos = () => {
               <option value="99.9">99.9</option>
               <option value="99.99">99.99</option>
             </select>
-            <label for="tipoCarretera">Tipo de carretera:</label>
-            <select name="tipoCarretera" id="tipoCarretera" required>
+            <label htmlFor="tipoCarretera">Tipo de carretera:</label>
+            <select
+              name="tipoCarretera"
+              id="tipoCarretera"
+              value={tipoCarretera_}
+              onChange={(e) => handleChange(e, setTipoCarretera)}
+              required
+            >
               <option value="grande">Grande</option>
               <option value="pequena">Pequeña</option>
             </select>
-            <label for="m">Coeficiente de drenaje:</label>
+            <label htmlFor="m">Coeficiente de drenaje:</label>
             <input
               type="number"
               min="0.40"
@@ -78,11 +277,13 @@ const Pavimentos = () => {
               step="0.05"
               id="m"
               placeholder="Ej: 0.9"
+              value={m_}
+              onChange={(e) => handleChange(e, setM)}
               required
             />
           </div>
           <div>
-            <label for="modulo1">Módulo de capa asfáltica (MPa):</label>
+            <label htmlFor="modulo1">Módulo de capa asfáltica (MPa):</label>
             <input
               type="number"
               id="modulo1"
@@ -90,9 +291,11 @@ const Pavimentos = () => {
               max="10000"
               step="50"
               placeholder="Ej: 4000"
+              value={moduloCapa1_}
+              onChange={(e) => handleChange(e, setModuloCapa1)}
               required
             />
-            <label for="modulo2">Módulo de base granular (MPa):</label>
+            <label htmlFor="modulo2">Módulo de base granular (MPa):</label>
             <input
               type="number"
               id="modulo2"
@@ -100,9 +303,11 @@ const Pavimentos = () => {
               max="600"
               step="5"
               placeholder="Ej: 300"
+              value={moduloCapa2_}
+              onChange={(e) => handleChange(e, setModuloCapa2)}
               required
             />
-            <label for="modulo3">Módulo de subbase granular (MPa):</label>
+            <label htmlFor="modulo3">Módulo de subbase granular (MPa):</label>
             <input
               type="number"
               id="modulo3"
@@ -110,9 +315,11 @@ const Pavimentos = () => {
               max="600"
               step="5"
               placeholder="Ej: 150"
+              value={moduloCapa3_}
+              onChange={(e) => handleChange(e, setModuloCapa3)}
               required
             />
-            <label for="modulo4">Módulo de subrasante (MPa):</label>
+            <label htmlFor="modulo4">Módulo de subrasante (MPa):</label>
             <input
               type="number"
               id="modulo4"
@@ -120,12 +327,14 @@ const Pavimentos = () => {
               max="100"
               step="1"
               placeholder="Ej: 40"
+              value={moduloCapa4_}
+              onChange={(e) => handleChange(e, setModuloCapa4)}
               required
             />
           </div>
         </form>
         <div className="botones">
-          <button type="button" id="boton-calcular">
+          <button type="button" id="boton-calcular" onClick={() => calcular()}>
             Calcular
           </button>
           <button type="button" id="boton-recuperar">
